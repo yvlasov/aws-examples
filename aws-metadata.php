f3ac01702551f3412f7b9cca27324353cc22f5f3
yvlasov <yvlasov@pytn.ru>
2016-07-28 11:50:17 +0300

    initial commit

---
+++ aws-metadata.php
@@ -0,0 +1,8 @@
+<?php
+      $url = "http://169.254.169.254/latest/meta-data/instance-id";
+      $instance_id = file_get_contents($url);
+      echo "Instance ID: <b>" . $instance_id . "</b><br/>";
+      $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+      $zone = file_get_contents($url);
+      echo "Zone: <b>" . $zone . "</b><br/>";
+?>
