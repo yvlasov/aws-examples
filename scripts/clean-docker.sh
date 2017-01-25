475cb120a95d0385752b2a787a110d3d88c8abef
yvlasov <yvlasov@users.noreply.github.com>
2017-01-25 18:44:37 +0400

    Update clean-docker.sh

--- scripts/clean-docker.sh
+++ scripts/clean-docker.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 df -h 
 
-docker rm $(docker ps -a --filter "status=exited" --no-trunc -aq 2> /dev/null ) 2> /dev/null
+if [ x$( docker ps -a --filter "status=exited" --no-trunc -aq 2> /dev/null | wc -l ) != "x0" ]; then
+	docker rm $(docker ps -a --filter "status=exited" --no-trunc -aq 2> /dev/null ) 2> /dev/null
+fi
 
-docker rmi $(docker images -q -f "dangling=true" --no-trunc 2> /dev/null ) 2> /dev/null
+if [ x$( docker images -q -f "dangling=true" --no-trunc 2> /dev/null | wc -l ) != "x0" ]; then
+	docker rmi $(docker images -q -f "dangling=true" --no-trunc 2> /dev/null ) 2> /dev/null
+fi
 
-DELETE_MIN_TIME_GAP=$[ 3600 * 8 ];
-DELETE_MAX_TIME_GAP=$[ 3600 * 24 * 3 ];
 CUR_DATE=$(date +%s)
-docker images --no-trunc -aq 2> /dev/null | \
-while read line
+DELETE_MIN_TIME_GAP=$[ $CUR_DATE - ( 3600 * 24 ) ];
+DELETE_MAX_TIME_GAP=$[ $CUR_DATE - ( 3600 * 24 * 5 ) ];
+
+#WHILE LOOP sh compatible
+docker images --no-trunc -aq 2> /dev/null | while read line
 do
+  if [ x$line != "x" ]; then  
     CREATE_TIMESTAMP=0
-    echo "Inspecting image: $line"
+    #echo "Inspecting image: $line"
     CREATE_TIMESTAMP=`date --date="$( docker inspect -f '{{ .Created }}' $line 2> /dev/null )" +%s`
-    if [ $CREATE_TIMESTAMP -le $[ $CUR_DATE - $DELETE_MIN_TIME_GAP ] ] && [ $CREATE_TIMESTAMP -gt $[ $CUR_DATE - $DELETE_MAX_TIME_GAP ] ]; then
+    if [ x$CREATE_TIMESTAMP != "x" ] && [ $CREATE_TIMESTAMP -le $DELETE_MIN_TIME_GAP ] && [ $CREATE_TIMESTAMP -gt $DELETE_MAX_TIME_GAP ]; then
       echo "Deleting image: $line"
-      docker rmi -f $line 2> /dev/null
+      docker rmi -f $line 
     fi
-done 
+  fi
+done
 
 df -h
